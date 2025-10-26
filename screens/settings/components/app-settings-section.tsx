9cbc98044978ef35415d8e6c547e6cdd9588ae25
adityakmr7 <adityakmr9672@gmail.com>
2025-10-27 00:30:52 +0530

    fix: remove cloud sync

--- screens/settings/components/app-settings-section.tsx
+++ screens/settings/components/app-settings-section.tsx
@@ -1,73 +1,56 @@
-import { useSettingsStore } from "@/stores/setting-store";
-import React from "react";
-import { Switch } from "react-native-heroui";
-import ChevronRight from "./chevron-right";
-import Divider from "./divider";
-import SettingItem from "./setting-item";
-import SettingsSection from "./settings-section";
+import { useSettingsStore } from '@/stores/setting-store';
+import React from 'react';
+import { Switch } from 'react-native-heroui';
+import ChevronRight from './chevron-right';
+import Divider from './divider';
+import SettingItem from './setting-item';
+import SettingsSection from './settings-section';
 
 interface AppSettingsSectionProps {
-  onTextSizePress: () => void;
-  onThemePress: () => void;
+    onTextSizePress: () => void;
+    onThemePress: () => void;
 }
 
 const AppSettingsSection: React.FC<AppSettingsSectionProps> = ({
-  onTextSizePress,
-  onThemePress,
+    onTextSizePress,
+    onThemePress,
 }) => {
-  const {
-    themeMode,
-    notifications,
-    setNotifications,
-    textSize,
-    syncWithCloud,
-    setSyncWithCloud,
-  } = useSettingsStore();
+    const {
+        themeMode,
+        notifications,
+        setNotifications,
+        textSize,
+        syncWithCloud,
+        setSyncWithCloud,
+    } = useSettingsStore();
 
-  return (
-    <SettingsSection title="App Settings">
-      <SettingItem
-        title="Notifications"
-        subtitle="Get notified when timer completes in background"
-        rightElement={
-          <Switch size="md" value={notifications} onChange={setNotifications} />
-        }
-      />
-      <Divider />
-      <SettingItem
-        title="Text Size"
-        subtitle={textSize.charAt(0).toUpperCase() + textSize.slice(1)}
-        rightElement={<ChevronRight />}
-        onPress={onTextSizePress}
-      />
-      <Divider />
-      <SettingItem
-        title="Theme"
-        subtitle={
-          themeMode === "system"
-            ? "System"
-            : themeMode === "light"
-              ? "Light"
-              : "Dark"
-        }
-        rightElement={<ChevronRight />}
-        onPress={onThemePress}
-      />
-      <Divider />
-      <SettingItem
-        disabled={true}
-        title="Sync with Cloud"
-        rightElement={
-          <Switch
-            isDisabled={true}
-            size="md"
-            value={syncWithCloud}
-            onChange={setSyncWithCloud}
-          />
-        }
-      />
-    </SettingsSection>
-  );
+    return (
+        <SettingsSection title="App Settings">
+            <SettingItem
+                title="Notifications"
+                subtitle="Get notified when timer completes in background"
+                rightElement={
+                    <Switch size="md" value={notifications} onChange={setNotifications} />
+                }
+            />
+            <Divider />
+            <SettingItem
+                title="Text Size"
+                subtitle={textSize.charAt(0).toUpperCase() + textSize.slice(1)}
+                rightElement={<ChevronRight />}
+                onPress={onTextSizePress}
+            />
+            <Divider />
+            <SettingItem
+                title="Theme"
+                subtitle={
+                    themeMode === 'system' ? 'System' : themeMode === 'light' ? 'Light' : 'Dark'
+                }
+                rightElement={<ChevronRight />}
+                onPress={onThemePress}
+            />
+        </SettingsSection>
+    );
 };
 
 export default AppSettingsSection;
